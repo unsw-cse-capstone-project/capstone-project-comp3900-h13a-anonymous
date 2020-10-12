a8f83408c7b083bb1114ece653ae2efe8be36208
Haoyang Ren <ryanreninau@gmail.com>
2020-10-12 16:07:04 +1100

    Create user table and interaction with database

--- buy_sell.py
+++ buy_sell.py
@@ -1,13 +1,20 @@
 from search import search
 import json
-def buy(name, amount):
+import sqlite3
+
+def buy(name, amount, id):
     tgt = search(name)
     stockinfo = json.loads(tgt)
 
     price = stockinfo["data"][0]['p']
     print(price)
     money = price * amount
+
+    conn = sqlite3.connect('hermes.db')
     #interact with database to subtract money
+    conn.execute("UPDATE USER set BALANCE = BALANCE - ? where ID = ?", (money,id))
+    conn.commit()
+    print ("Total number of rows updated :", conn.total_changes)
     #interact with database to add stock to user's account
 
 # def sell(name, amount):
@@ -18,4 +25,4 @@ def buy(name, amount):
 #         price = stockinfo["data"][0]['p']
 #         money = price * amount
 if __name__ == "__main__":
-    buy("BINANCE:BTCUSDT", 1)
+    buy("BINANCE:BTCUSDT", 1, 1)
--- database.py
+++ database.py
@@ -3,12 +3,25 @@ import sqlite3
 conn = sqlite3.connect('hermes.db')
 print ("Opened database successfully")
 
+conn.execute('''DROP TABLE STOCK''')
+conn.execute('''DROP TABLE USER''')
+
 conn.execute('''CREATE TABLE STOCK
          (CODE TEXT PRIMARY KEY NOT NULL,
          PRICE             REAL NOT NULL,
          TIMESTAMP      INTEGER NOT NULL,
          VLOUMN           REAL);''')
-print ("Table created successfully")
+print ("Stock table created successfully")
 
+conn.execute('''CREATE TABLE USER
+         (ID INTEGER PRIMARY KEY autoincrement,
+         EMAIL              TEXT NOT NULL,
+         PASSWORD           TEXT NOT NULL,
+         PHONE           INTEGER NOT NULL,
+         BALANCE            REAL NOT NULL,
+         NAME               TEXT NOT NULL,
+         LOGIN              TEXT NOT NULL,
+         LOGOUT             TEXT NOT NULL);''')
+print ("User table created successfully")
 conn.commit()
 conn.close()
