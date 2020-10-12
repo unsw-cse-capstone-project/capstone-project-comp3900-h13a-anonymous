d09bc57d38b3e2e019fd0c740c5393f8f529ca8d
Haoyang Ren <ryanreninau@gmail.com>
2020-10-12 16:55:19 +1100

    Append Watchlist and Purchase table to the database

--- database.py
+++ database.py
@@ -5,6 +5,8 @@ print ("Opened database successfully")
 
 conn.execute('''DROP TABLE STOCK''')
 conn.execute('''DROP TABLE USER''')
+conn.execute('''DROP TABLE WATCHLIST''')
+conn.execute('''DROP TABLE PURCHASE''')
 
 conn.execute('''CREATE TABLE STOCK
          (CODE TEXT PRIMARY KEY NOT NULL,
@@ -15,13 +17,33 @@ print ("Stock table created successfully")
 
 conn.execute('''CREATE TABLE USER
          (ID INTEGER PRIMARY KEY autoincrement,
-         EMAIL              TEXT NOT NULL,
+         EMAIL              TEXT NOT NULL UNIQUE,
          PASSWORD           TEXT NOT NULL,
-         PHONE           INTEGER NOT NULL,
+         PHONE           INTEGER NOT NULL UNIQUE,
          BALANCE            REAL NOT NULL,
          NAME               TEXT NOT NULL,
          LOGIN              TEXT NOT NULL,
          LOGOUT             TEXT NOT NULL);''')
 print ("User table created successfully")
+
+conn.execute('''CREATE TABLE WATCHLIST
+         (ID                 INTEGER NOT NULL UNIQUE,
+         CODE                TEXT NOT NULL,
+         DATEADD             DATE,
+         FOREIGN KEY (ID) REFERENCES USER (ID),
+         FOREIGN KEY (CODE) REFERENCES STOCK (CODE));''')
+print ("Watchlist table created successfully")
+
+conn.execute('''CREATE TABLE PURCHASE
+         (ID                 INTEGER NOT NULL UNIQUE,
+         CODE                TEXT NOT NULL,
+         DATEBUY             DATE,
+         DATESELL            DATE,
+         UNITBUY             REAL,
+         UNITSELL            REAL,
+         FOREIGN KEY (ID) REFERENCES USER (ID),
+         FOREIGN KEY (CODE) REFERENCES STOCK (CODE));''')
+print ("Purchase table created successfully")
+
 conn.commit()
 conn.close()
