f8b163663529ad9c4d75ff5f655a2f2ed5bdf072
haodong lu <lhd32262175@gmail.com>
2020-10-13 15:47:52 +1100

    histo data done, dynamic html graph done

--- api/historical.py
+++ api/historical.py
@@ -1,25 +1,29 @@
 ########################
 ## pip3 install plotly
 
-
-# import requests
-# r = requests.get('https://finnhub.io/api/v1/stock/candle?symbol=AAPL&resolution=1&from=1572651390&to=1572910590&token=btkkvsv48v6r1ugbcp70')
-# print(r.json())
-
-
+import requests
+from datetime import datetime
 import plotly.graph_objects as go
 import pandas as pd
-
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
-
-fig = go.Figure(data=[go.Candlestick(x=df['Date'],
-                open=df['AAPL.Open'], high=df['AAPL.High'],
-                low=df['AAPL.Low'], close=df['AAPL.Close'])
-                     ])
-
-fig.update_layout(xaxis_rangeslider_visible=False)
-fig.show()
-
 import plotly.express as px
 
-fig.write_html("sample_historical_data.html")
+def get_historical(code, time, path):
+     now =  datetime.now().timestamp()
+     df = pd.read_csv(f'https://finnhub.io/api/v1/stock/candle?symbol={code}&resolution=1&from={time}&to={now}&token=btkkvsv48v6r1ugbcp70&format=csv')
+
+     print(df)
+
+     date = []
+     for d in df['t']:
+          date.append(datetime.fromtimestamp(d))
+     fig = go.Figure(data=[go.Candlestick(x=date,
+                    open=df['o'], high=df['h'],
+                    low=df['l'], close=df['c'])
+                         ])
+
+     fig.update_layout(xaxis_rangeslider_visible=False)
+
+     fig.write_html("sample_historical_data.html")
+
+if __name__ == "__main__":
+     get_historical("AAPL", 1601577795, "sample_historical_data.html")
