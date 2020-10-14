7c046bf0fc2ea45c95641299e6771693088e0027
Saloni <z5215272@student.unsw.edu.au>
2020-10-14 18:59:46 +1100

    Added login functionality

--- simulator/urls.py
+++ simulator/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     # 127.0.0.1:8000
@@ -8,4 +8,7 @@ urlpatterns = [
 
     # 127.0.0.1:8000/stock/1
     path('stock/<int:pk>/', views.stock_detail, name='stock_detail'),
+
+    # 127.0.0.1:8000/accounts/login
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login')
 ]
--- simulator/views.py
+++ simulator/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Stock 
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def stock_list(request):
     stocks = Stock.objects.all()
     frontend_stocks = {'stocks': stocks}
     return render(request, 'simulator/stock_list.html', frontend_stocks)
 
+@login_required
 def stock_detail(request, pk):
     stock = get_object_or_404(Stock, pk=pk)
     frontend_stock = {'stock': stock}
