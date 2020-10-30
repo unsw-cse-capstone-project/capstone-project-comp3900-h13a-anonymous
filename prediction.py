69104b05abcc28d60735a573bd359c96d1b73bda
Taelot <sdsqianxrfxdho@163.com>
2020-10-31 03:15:05 +1100

    'change_nameofhistorical2.pyAndaddingPredictionFunctioh2.py

---
+++ prediction.py
@@ -0,0 +1,22 @@
+from sklearn import linear_model
+from api.historical2 import *
+import numpy as np
+from datetime import date, timedelta
+def prediction(code, day):
+    result = []
+    reg = linear_model.BayesianRidge()
+    data = get_historical_data(code,1572447323)
+    x = []
+    for i in range(0, len(data)):
+        x.append(i)
+    y = [0]*len(data)
+    newX = np.vstack((x,y)).T
+    reg.fit(newX,data)
+    for i in range(0,day):
+        da = date.today() + timedelta(days=i+1)
+        pre = float(reg.predict([[i+1+len(data),0]]))
+        result.append((da,pre))
+    print(result)
+    return result
+if __name__ == "__main__":
+    prediction("AAPL", 30)
