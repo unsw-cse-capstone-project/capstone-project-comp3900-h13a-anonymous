b52777add32b21a0781deeb35ad670837f740fc5
haodong lu <lhd32262175@gmail.com>
2020-11-07 23:10:52 +1100

    django test init

---
+++ buysellTest.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from buy_sell import *
+
+class buysellTest(TestCase):
+    def setUp(self):
+        ## set up code here
+        pass
+
+    def test_1(self):
+        assert 1==1
+
---
+++ watchpriceTest.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from watchprice_2 import Watchprice
+
+class buysellTest(TestCase):
+    def setUp(self):
+        ## set up code here
+        pass
+
+    def test_1(self):
+        assert 1==1
+
