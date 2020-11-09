fd7668aa564542095c6034a035ee623b30c05573
Taelot <sdsqianxrfxdho@163.com>
2020-11-09 16:58:58 +1100

    Complete prediction algorithm

--- prediction.py
+++ prediction.py
@@ -3,6 +3,7 @@ from api.historical2 import *
 import numpy as np
 import random
 from datetime import date, timedelta
+import time
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -10,22 +11,50 @@ ssl._create_default_https_context = ssl._create_unverified_context
 def predict(code, day):
     result = []
     reg = linear_model.BayesianRidge()
-    data = get_historical_data(code, 1572447323)
+    reg2 = linear_model.BayesianRidge()
+    reg3 = linear_model.BayesianRidge()
+    reg4 = linear_model.BayesianRidge()
+    today = datetime.today()
+    tgt = today - timedelta(days=day)
+    ts = time.mktime(tgt.timetuple())
+    data = get_historical_data(code, ts)
+    data1 = data[0]
+    data2 = data[1]
+    data3 = data[2]
+    data4 = data[3]
     x = []
-    for i in range(0, len(data)):
+    for i in range(0, len(data1)):
         x.append(i)
-    y = [0]*len(data)
+    y = [0]*len(data1)
     newX = np.vstack((x,y)).T
-    reg.fit(newX,data)
+    reg.fit(newX,data1)
+    x = []
+    for i in range(0, len(data1)):
+        x.append(i)
+    y = [0]*len(data2)
+    newX = np.vstack((x,y)).T
+    reg2.fit(newX,data2)
+    x = []
+    for i in range(0, len(data1)):
+        x.append(i)
+    y = [0]*len(data3)
+    newX = np.vstack((x,y)).T
+    reg3.fit(newX,data3)
+    x = []
+    for i in range(0, len(data4)):
+        x.append(i)
+    y = [0]*len(data4)
+    newX = np.vstack((x,y)).T
+    reg4.fit(newX,data4)
 
     for i in range(0,day):
         da = date.today() + timedelta(days=i+1)
-        sb =  random.uniform(-3,2)
-        pre = float(reg.predict([[i+1+len(data),0]])) + sb
+        #sb =  random.uniform(-3,2)
+        pre = float(reg.predict([[i+1+len(data1),0]]) + reg2.predict([[i+1+len(data1),0]]) - reg3.predict([[i+1+len(data1),0]]) + reg4.predict([[i+1+len(data1),0]]))/2
         result.append((da,pre))
-    # print(result)
+    print(result)
     return result
 
 
 if __name__ == "__main__":
-    predict("AAPL", 30)
+    predict("BABA", 30)
