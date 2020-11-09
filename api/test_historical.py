9ce375c0b16f541d68eeee871a60eb9812802aec
haodong lu <lhd32262175@gmail.com>
2020-11-09 21:35:53 +1100

    test refactor

--- api/test_historical.py
+++ api/test_historical.py
@@ -1,13 +1,16 @@
 from api.historical2 import get_historical
 from datetime import datetime
+from django.test import TestCase
 
-now = datetime.now().timestamp()
+class test_historical(TestCase):
+    def setUp(self):
+        self.now = datetime.now().timestamp()
 
-def test_upper():
-    assert get_historical('AAPL', now) is not None
+    def test_upper(self):
+        assert get_historical('AAPL', self.now) is not None
 
-def test_lower():
-    assert get_historical('aapl', now) is not None
+    def test_lower(self):
+        assert get_historical('aapl', self.now) is not None
 
-def test_invalidName():
-    assert get_historical('apl', now) is None
+    def test_invalidName(self):
+        assert get_historical('apl', self.now) is None
--- api/test_search.py
+++ api/test_search.py
@@ -12,16 +12,20 @@
 
 
 from api.search_v2 import Api
+from django.test import TestCase
 
-api = Api()
+class test_search(TestCase):
+    def setUp(self):
+        self.api = Api()
 
-def test_upper():
-    assert api.search("AAPL") != {}
 
-def test_lower():
-    assert api.search("aapl") != {}
+    def test_upper(self):
+        assert self.api.search("AAPL") != {}
 
-def test_invalid():
-    assert api.search("abcd") == "The stock code you searched was invalid"
-    assert api.search("goog") == "The stock code you searched was invalid"
+    def test_lower(self):
+        assert self.api.search("aapl") != {}
+
+    def test_invalid(self):
+        assert self.api.search("abcd") == "The stock code you searched was invalid"
+        assert self.api.search("goog") == "The stock code you searched was invalid"
     
